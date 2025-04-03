07888fd048ea59146be604a3ac2cd264331d85fc
Nikhil Gupta <114578563+rhizun2@users.noreply.github.com>
2025-04-03 14:24:10 +0530

    Update TwoSum.cpp

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-    unordered_map<int, int> seen;
-    for (int i = 0; i < nums.size(); i++) {
-        int complement = target - nums[i];
-        if (seen.count(complement)) {
-            return {seen[complement], i};
+        unordered_map<int, int> seen;
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (seen.count(complement)) {
+                return {seen[complement], i};
+            }
+            seen[nums[i]] = i;
         }
-        seen[nums[i]] = i;
-    }
-    return {};  
-    }
-};
+        return {};  
+        }
+    };
