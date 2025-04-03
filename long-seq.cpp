641408cf96011beefe1796787876b069fe398e67
Nikhil Gupta <114578563+rhizun2@users.noreply.github.com>
2025-04-03 13:11:02 +0530

    Update long-seq.cpp

--- long-seq.cpp
+++ long-seq.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        unordered_set<int> numMp(nums.begin(), nums.end());
+        unordered_map<int, int> mp;
         int longest = 0;
 
-        for (int num : numMp) {
-            if (!numMp.count(num - 1)) { 
-                int cNum = num;
-                int sk = 1;
-                while (numMp.count(cNum + 1)) {
-                    cNum++;
-                    sk++;
-                }
-                longest = max(longest, sk);
-             }
+        for (int num : nums) {
+            if (mp.find(num) == mp.end()) {  
+                int left = mp.count(num - 1) ? mp[num - 1] : 0;
+                int right = mp.count(num + 1) ? mp[num + 1] : 0;
+                int streak = left + right + 1;
+
+                mp[num] = streak;
+                mp[num - left] = streak;  
+                mp[num + right] = streak; 
+
+                longest = max(longest, streak);
+            }
         }
         return longest;
     }
